7ec3a684ad3648a06fbaa5c4f7985273e6e6892d
DeveloperECC <elmercabrerac5@gmail.com>
2025-10-05 17:30:24 +0000

    imagenes

--- src/servicios/parquesApi.js
+++ src/servicios/parquesApi.js
@@ -10,8 +10,8 @@ const parquesData = [
         galeria: [
             // Puedes añadir más imágenes de tu galería si tienes. Por ahora, mezclo tuyas y externas.
             '/assets/images/barracuda.png', // Usando una imagen de pez para la galería (ejemplo)
-            'https://upload.wikimedia.org/wikipedia/commons/e/ec/Playa_de_Cabo_San_Juan_en_el_Parque_Nacional_Natural_Tayrona.jpg',
-            'https://viajes.nationalgeographic.com.es/medio/2019/06/19/tayrona_468f742f_800x800.jpg',
+            'https://www.google.com/imgres?q=parque%20nacional%20tayrona&imgurl=https%3A%2F%2Fcolombia.travel%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fimagen_650x450_escala_y_recorte%2Fpublic%2Factividades%2FParque%2520Nacional%2520Natural%2520Tayrona%25205%252C%2520Foto%2520Charly%2520Boillot%2520%25281%2529%2520%25281%2529.jpg%3Fitok%3DRHA-BCVe&imgrefurl=https%3A%2F%2Fcolombia.travel%2Fes%2Fsanta-marta%2Fel-parque-tayrona&docid=yxETf6XWl27r1M&tbnid=_E25AYjeASnNuM&vet=12ahUKEwiu2NLbx42QAxVvSjABHWtFFaQQM3oECBoQAA..i&w=650&h=450&hcb=2&ved=2ahUKEwiu2NLbx42QAxVvSjABHWtFFaQQM3oECBoQAA',
+            'https://www.google.com/imgres?q=parque%20nacional%20tayrona&imgurl=https%3A%2F%2Fhotelparquetayrona.com%2Fwp-content%2Fuploads%2F2025%2F06%2Fcomo-llegar-al-parque-tayrona.jpg&imgrefurl=https%3A%2F%2Fhotelparquetayrona.com%2Fguia-completa-para-visitar-el-parque-tayrona%2F%3Fsrsltid%3DAfmBOopK3-nubW7ku4QqYfgcYLtAdRxK8raPaptK9eKhmdbDfYpwTNW6&docid=Zs3_4LfDfR1S8M&tbnid=IUlF1TTIPiBvdM&vet=12ahUKEwiu2NLbx42QAxVvSjABHWtFFaQQM3oFCIQBEAA..i&w=1200&h=800&hcb=2&ved=2ahUKEwiu2NLbx42QAxVvSjABHWtFFaQQM3oFCIQBEAA',
         ],
         peces: [
             { id: '1', nombre: 'Barracuda', imagen: '/assets/images/barracuda.png', dificultad: 6 },
@@ -24,7 +24,7 @@ const parquesData = [
         nombre: 'Parque Nacional Natural Los Nevados',
         ubicacion: 'Caldas, Risaralda, Quindío, Tolima',
         descripcion: 'Zona de alta montaña con picos nevados, páramos y bosques andinos. Sus lagunas y ríos de deshielo son el hogar de especies adaptadas a las bajas temperaturas, ofreciendo paisajes únicos de frailejones y valles glaciares.',
-        portada: '/assets/images/losNevados-portada.png', // Usando tu imagen de portada de Los Nevados
+        portada: '/assets/images/losNevados-portada.jpg', // Usando tu imagen de portada de Los Nevados
         galeria: [
             '/assets/images/trucha_andina.png', // Usando una imagen de pez para la galería (ejemplo)
             'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Nevado_del_Ruiz%2C_Colombia.jpg/1280px-Nevado_del_Ruiz%2C_Colombia.jpg',
@@ -40,7 +40,7 @@ const parquesData = [
         nombre: 'Parque Nacional Natural Corales del Rosario y de San Bernardo',
         ubicacion: 'Bolívar, Sucre',
         descripcion: 'Archipiélago coralino en el Caribe, con gran diversidad marina y playas paradisíacas. Sus arrecifes de coral son ecosistemas vitales para cientos de especies de peces tropicales, moluscos y tortugas marinas.',
-        portada: '/assets/images/corelesDelRosario-portada.png', // Usando tu imagen de portada de Corales del Rosario
+        portada: '/assets/images/coralesDelRosario-portada.jpg', // Usando tu imagen de portada de Corales del Rosario
         galeria: [
             '/assets/images/cirujano_azul.png', // Usando una imagen de pez para la galería (ejemplo)
             'https://upload.wikimedia.org/wikipedia/commons/e/ec/Playa_en_Islas_del_Rosario%2C_Colombia.jpg',
@@ -57,7 +57,7 @@ const parquesData = [
         nombre: 'Parque Nacional Natural Amacayacu',
         ubicacion: 'Amazonas',
         descripcion: 'Parque amazónico dedicado a la conservación de la selva tropical y sus comunidades indígenas. Atraviesado por el río Amazonas, es un santuario de delfines rosados, pirañas y una inmensa variedad de peces de agua dulce.',
-        portada: '/assets/images/amacayacu-portada.png', // Usando tu imagen de portada de Amacayacu
+        portada: '/assets/images/amacayacu-portada.jpg', // Usando tu imagen de portada de Amacayacu
         galeria: [
             '/assets/images/piraña_roja.png', // Usando una imagen de pez para la galería (ejemplo)
             'https://upload.wikimedia.org/wikipedia/commons/thumb/d/d4/Laguna_de_Tarapoto_Amazonas.jpg/1280px-Laguna_de_Tarapoto_Amazonas.jpg',
@@ -74,7 +74,7 @@ const parquesData = [
         nombre: 'Parque Nacional Natural Serranía de Chiribiquete',
         ubicacion: 'Caquetá, Guaviare',
         descripcion: 'Patrimonio de la Humanidad, es un santuario de arte rupestre y biodiversidad. Aunque es de difícil acceso, sus ríos prístinos albergan especies únicas de la Amazonía, muchas aún por descubrir.',
-        portada: '/assets/images/chiribiquete-portada.png', // Usando tu imagen de portada de Chiribiquete
+        portada: '/assets/images/chiribiquete-portada.jpg', // Usando tu imagen de portada de Chiribiquete
         galeria: [
             '/assets/images/Arapaima.png', // Usando una imagen de pez para la galería (ejemplo)
             'https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Serran%C3%ADa_de_Chiribiquete.jpg/1280px-Serran%C3%ADa_de_Chiribiquete.jpg',
